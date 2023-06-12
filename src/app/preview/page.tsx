4a23077a407270686b3396718a57e9b86427e219
style77 <stylek777@gmail.com>
2023-06-12 15:49:44 +0200

    feat: mobile preview page

---
+++ src/app/preview/page.tsx
@@ -0,0 +1,17 @@
+export default function Preview({
+    searchParams,
+  }: {
+    searchParams: {
+      website: string;
+    };
+  }) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-800">
+        <iframe
+          id="mobile-iframe"
+          src={`https://${searchParams.website}`}
+          className="w-full h-screen md:w-[414px] md:h-[896px]"
+        ></iframe>
+      </main>
+    );
+  }
