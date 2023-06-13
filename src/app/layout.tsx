35bc9b73d9032b9a7a7628183baaa1628d2a1b2e
style77 <stylek777@gmail.com>
2023-06-13 19:57:21 +0200

    chore: seo improvements - canonical

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,35 @@
+import { Metadata } from 'next'
 import Footer from './components/Footer'
 import './globals.css'
 import { Raleway } from 'next/font/google'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Codenerds - Development and Design Agency',
   description: 'Codenerds is a leading development and design agency. We specialize in creating innovative and custom solutions to turn your dream projects into reality.',
+  alternates: {
+    canonical: 'https://codenerds.tech/',
+  },
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+    other: {
+      rel: "favicon",
+      url: "/favicon.png"
+    }
+  },
+  appleWebApp: {
+    statusBarStyle: "black-translucent"
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true
+    }
+  }
 }
 
 const raleway = Raleway({
